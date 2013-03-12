75155966d6cc8f2cb27f6fc66fcdd0b6a63dc030
chris-r <chris.ross@entopa.jp>
2013-03-12 16:25:18 +0900

    Update dazzle.sh

--- dazzle.sh
+++ dazzle.sh
@@ -180,6 +180,9 @@ create_project () {
     echo "  -> chown --recursive $DAZZLE_USER:$DAZZLE_GROUP $DAZZLE_HOME"
     chown --recursive $DAZZLE_USER:$DAZZLE_GROUP "$DAZZLE_HOME"
 
+    echo "  -> chmod --recursive o-rwx $DAZZLE_HOME/$1"
+    chmod --recursive o-rwx "$DAZZLE_HOME"/"$1"
+
     sleep 0.5
 
     echo
